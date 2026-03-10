3927e5895a948259c8c4ed89ef7dd21363e08ce0
JonArrausi <jon.arrausi@estudiantat.upc.edu>
2026-03-10 20:09:38 +0100

    Update MarcaEnEventoDeCampo.java

--- decathlon-competition/MarcaEnEventoDeCampo.java
+++ decathlon-competition/MarcaEnEventoDeCampo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.upc.etsetb.poo.decathlon1.dominio;
 
 /**
@@ -26,4 +22,5 @@ public abstract class MarcaEnEventoDeCampo extends MarcaEnEvento{
     }
     
 }
+
 
