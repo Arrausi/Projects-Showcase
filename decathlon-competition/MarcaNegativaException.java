9fa056486fe3480ebb716234b5de78007ed2b17d
JonArrausi <jon.arrausi@estudiantat.upc.edu>
2026-03-10 20:02:50 +0100

    Remove unnecessary comments from MarcaNegativaException

--- decathlon-competition/MarcaNegativaException.java
+++ decathlon-competition/MarcaNegativaException.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.upc.etsetb.poo.decathlon1.dominio;
 
 /**
@@ -21,4 +18,5 @@ public class MarcaNegativaException extends Exception{
     }
     
 }
+
 
