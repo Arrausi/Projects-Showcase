84f9c5f5eba196b51b422685a56e8913b86b3475
JonArrausi <jon.arrausi@estudiantat.upc.edu>
2026-03-10 20:09:05 +0100

    Update MarcaEnEvento100m.java

--- decathlon-competition/MarcaEnEvento100m.java
+++ decathlon-competition/MarcaEnEvento100m.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.upc.etsetb.poo.decathlon1.dominio;
 
 /**
@@ -34,4 +30,5 @@ public class MarcaEnEvento100m extends MarcaEnEventoDePista{
     }
     
 }
+
 
