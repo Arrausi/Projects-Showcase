a345f38e52fb37736fb98dd874204f52dc520cc9
JonArrausi <jon.arrausi@estudiantat.upc.edu>
2026-03-10 20:05:03 +0100

    Update NumInscripcionException.java

--- decathlon-competition/NumInscripcionException.java
+++ decathlon-competition/NumInscripcionException.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.upc.etsetb.poo.decathlon1.dominio;
 
 /**
  *
- * @author jonar
  */
 public class NumInscripcionException extends Exception{
     
@@ -23,3 +18,4 @@ public class NumInscripcionException extends Exception{
     }
     
 }
+
