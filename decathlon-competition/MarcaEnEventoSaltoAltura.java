91165edbc7e5d59b6eadf968d75d36c1ebada3ab
JonArrausi <jon.arrausi@estudiantat.upc.edu>
2026-03-10 20:10:30 +0100

    Update MarcaEnEventoSaltoAltura.java

--- decathlon-competition/MarcaEnEventoSaltoAltura.java
+++ decathlon-competition/MarcaEnEventoSaltoAltura.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.upc.etsetb.poo.decathlon1.dominio;
 
 /**
@@ -30,4 +26,5 @@ public class MarcaEnEventoSaltoAltura extends MarcaEnEventoDeCampo{
     }
     
 }
+
 
